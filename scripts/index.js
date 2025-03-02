882777aa57d0ef80826a7aacbb918f7fa09774b6
Pavel Goloveiko <iwtf@yandex.ru>
2025-03-02 22:46:50 +0300

    small fix

--- scripts/index.js
+++ scripts/index.js
@@ -8,14 +8,15 @@ let cardList=document.querySelector('.places__list');
 function createCard(card, deleteFunction){
   let cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = card.name;
   cardElement.querySelector('.card__title').textContent = card.name;
   let deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click',deleteFunction)
   return cardElement;
 }
+
 //Функция удаления карточки
 function deleteCard(event) {
-  console.log(event.target);
   event.target.parentElement.remove();
 }
 
