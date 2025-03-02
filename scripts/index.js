6457f7a57902a19a3832ff20f0857f364e345605
Pavel Goloveiko <iwtf@yandex.ru>
2025-03-02 22:22:26 +0300

    finish?

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
-
+// @todo: Темплейт карточки initialCards
+let cardTemplate=document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+let cardList=document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function createCard(card, deleteFunction){
+  let cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__title').textContent = card.name;
+  let deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click',deleteFunction)
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(event) {
+  console.log(event.target);
+  event.target.parentElement.remove();
+}
 // @todo: Вывести карточки на страницу
+function renderCards(){
+  initialCards.forEach(item=>
+    cardList.append(createCard(item,deleteCard))
+  )
+}
+renderCards();
