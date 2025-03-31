cd9557e7ffa68b30a958422fd1f1378e7024c179
Pavel Goloveiko <iwtf@yandex.ru>
2025-03-31 22:54:43 +0300

    Плавное появление popup

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,6 +1,10 @@
 export function showPopup(element){
   element.classList.add('popup_is-opened');  
+  // element.classList.add('popup_is-animated');  
+  
 }
 export function closePopup(element){
-    element.classList.remove('popup_is-opened');
+  element.classList.add('popup_is-opened');  
+  element.classList.remove('popup_is-opened');
+    
 }
